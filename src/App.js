44f6a8d54b711bcab505519753b31a00c4c25f72
= <connorruff99@gmail.com>
2020-12-01 14:15:53 -0500

    test change

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Mike and Connor's Web App. Commong Soon...
         </p>
         <a
           className="App-link"
