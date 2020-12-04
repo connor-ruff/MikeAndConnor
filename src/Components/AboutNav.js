adf3f8ea3d6502cf60df10dd045ac349bccbf452
= <connorruff99@gmail.com>
2020-12-03 22:11:43 -0500

    commit

--- src/Components/AboutNav.js
+++ src/Components/AboutNav.js
@@ -14,6 +14,7 @@ class AboutNav extends Component {
                 <Nav.Link onClick={() => this.props.meetUs(0)}>About</Nav.Link>
                 <Nav.Link onClick={() => this.props.meetUs(1)}>Meet Mike</Nav.Link>
                 <Nav.Link onClick={() => this.props.meetUs(2)}>Meet Connor</Nav.Link>
+                <Nav.Link>Birding</Nav.Link>
                  </Nav>
             </Navbar>
             
--- src/index.js
+++ src/index.js
@@ -3,12 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {BrowserRouter, Route} from 'react-router-dom';
+import Birding from './Components/Birding';
+import Hockey from './Components/Hockey';
 
 ReactDOM.render(
+  <BrowserRouter>
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <Route path='/' exact component={App} />
+    <Route path='/bird' exact component={Birding} />
+    <Route path='/hockey' exact component={Hockey} />
+  </React.StrictMode>
+  </BrowserRouter>,
   document.getElementById('root')
+  
 );
 
 // If you want to start measuring performance in your app, pass a function
