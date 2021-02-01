31bf3d6291fb21a8794a62962d59808d44dd2d61
= <connorruff99@gmail.com>
2021-01-31 18:05:03 -0800

    Cali Update 1

---
+++ src/Constants/caliBirds.js
@@ -0,0 +1,73 @@
+const caliBirds = {
+    images: [
+        {
+         src: './BirdingPics/1.31.AcornWoodpecker.JPG',
+         height: 563,
+         bird: 'Acorn Woodpecker',
+         desc: '31 January 2021. Stanford University. Palo Alto, CA'
+        },
+        {
+            src: './BirdingPics/1.31.Nuttals.JPG',
+            height: 563,
+            bird: 'Nuttall\'s Woodpecker (female)',
+            desc: '31 January 2021. Menlo Park, CA'
+        },
+        {
+            src: './BirdingPics/1.31.WhiteBreastNut.JPG',
+            height: 563,
+            bird: 'White-Breasted Nuthatch',
+            desc: '31 January 2021. Stanford University. Palo Alto, CA'
+        },
+        {
+            src: './BirdingPics/1.31.HermitThrush.JPG',
+            height: 563,
+            bird: 'Hermit Thrush',
+            desc: '31 January 2021. Stanford University. Palo Alto, CA'
+        },
+        {
+        src: './BirdingPics/1.31.BlackPhoebe.JPG',
+        height: 563,
+        bird: 'Black Phoebe',
+        desc: '31 January 2021. Menlo Park, CA'
+        },
+        {
+            src: './BirdingPics/1.31.YellowRump.JPG',
+            height: 563,
+            bird: 'Yellow-Rumped Warbler',
+            desc: '31 January 2021. Stanford University. Palto Alto, CA'
+        },
+        {
+            src: './BirdingPics/1.31.AnnasHumming.JPG',
+            height: 563,
+            bird: 'Anna\'s Hummingbird',
+            desc: '(ID Confirmed By Sound) 31 January 2021. Menlo Park, CA'
+        }
+    ],
+
+    birdList: [
+        'Acorn Woodpecker',
+        'Nuttall\'s Woodpecker',
+        'White-Breasted Nuthatch',
+        'Hermit Thrush',
+        'Black Phoebe',
+        'Yellow-Rumped Warbler',
+        'Anna\'s Hummingbird',
+        'House Finch',
+        'Dark-Eyed Junco',
+        'California Scrub Jay',
+        'California Towhee', 
+        'Spotted Towhee',
+        'Chestnut-Backed Chickadee',
+        'Bewick\'s Wren',
+        'American Robin', 
+        'Mourning Dove',
+        'Eurasian-Collared Dove',
+        'Cedar Waxwing',
+        'Lesser Goldfinch',
+        'American Crow',
+        'Oak Titmouse',
+        'Ruby-Crowned Kinglet'
+    ]
+}
+
+export default caliBirds
