41e62e1e55de248b078df88330f9d97b49f1f82d
= <connorruff99@gmail.com>
2021-02-07 14:01:03 -0800

    Cali Update 2

--- src/Constants/caliBirds.js
+++ src/Constants/caliBirds.js
@@ -7,6 +7,12 @@ const caliBirds = {
          desc: '31 January 2021. Stanford University. Palo Alto, CA'
         },
         {
+            src: './BirdingPics/2.7.AnnasHumming.JPG',
+            height: 563,
+            bird: 'Anna\'s Hummingbird',
+            desc: '7 February 2021. East Palo Alto, CA'
+        },
+        {
             src: './BirdingPics/1.31.Nuttals.JPG',
             height: 563,
             bird: 'Nuttall\'s Woodpecker (female)',
@@ -17,6 +23,18 @@ const caliBirds = {
             height: 563,
             bird: 'White-Breasted Nuthatch',
             desc: '31 January 2021. Stanford University. Palo Alto, CA'
+        },
+        {
+            src: './BirdingPics/1.31.MourningDove.JPG',
+            height: 563,
+            bird: 'Mourning Dove',
+            desc: '31 January 2021. Stanford University. Palo Alto, CA'
+        },
+        {
+            src: './BirdingPics/2.7.GreaterYellowlegs.JPG',
+            height: 563,
+            bird: 'Greater Yellowlegs',
+            desc: '7 February 2021. East Palo Alto, CA'
         },
         {
             src: './BirdingPics/1.31.HermitThrush.JPG',
@@ -37,10 +55,22 @@ const caliBirds = {
             desc: '31 January 2021. Stanford University. Palto Alto, CA'
         },
         {
-            src: './BirdingPics/1.31.AnnasHumming.JPG',
+            src: './BirdingPics/2.7.Bushtit.JPG',
             height: 563,
-            bird: 'Anna\'s Hummingbird',
-            desc: '(ID Confirmed By Sound) 31 January 2021. Menlo Park, CA'
+            bird: 'Bushtit',
+            desc: '7 February 2021. East Palo Alto, CA'
+        },
+        {
+            src: './BirdingPics/2.7.NorthernShoveler.JPG',
+            height: 563,
+            bird: 'Northern Shoveler',
+            desc: '7 February 2021. East Palo Alto, CA'
+        },
+        {
+            src: './BirdingPics/2.7.SaysPhoebe.JPG',
+            height: 563,
+            bird: 'Say\'s Phoebe',
+            desc: '7 February 2021. East Palo Alto, CA'
         }
     ],
 
@@ -66,7 +96,15 @@ const caliBirds = {
         'Lesser Goldfinch',
         'American Crow',
         'Oak Titmouse',
-        'Ruby-Crowned Kinglet'
+        'Ruby-Crowned Kinglet',
+        'House Sparrow',
+        'Red-Tailed Hawk',
+        'Townsend\'s Warbler',
+        'Northern Shoveler',
+        'Say\'s Phoebe',
+        'Greater Yellowlegs',
+        'Bushtit',
+        'Golden-Crowned Sparrow'
     ]
 }
 
